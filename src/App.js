c74842978125a63fb4499a25f2e49a99a9af146a
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2023-04-24 23:02:31 -0700

    started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+const  App=()=> {
+  const [brews, setBrews] = useState([])
+  const [loading, setLoading] = useState(true);
+  useEffect(()=>{
+    const fetchData=()=>{
+      axios.get("https://api.openbrewerydb.org/v1/breweries?by_city=san_diego&per_page=3").then(
+        (response)=>{setBrews(response.data)}
+      
+      )
+    }
+    fetchData();
+    
+    setLoading(false)
+  },[])
+  if(loading===true){
+    return(
+      <h1>Loading...</h1>
+    )
+  }else{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  return(
+
+    <div className='App'>
+      {brews.map((brew)=>(
+        
+        
+  
+        
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="200"
+          image=""
+          alt=""
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {brew.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    ))}
     </div>
+      
   );
 }
 
+}
 export default App;
